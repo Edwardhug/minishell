f7909c18bfa4e44e3a66a56433e4f585f2b830e4
root <root@DESKTOP-7CO9QQK>
2023-09-27 14:20:32 +0200

    simple quote work

--- src/parsing/remove_things.c
+++ src/parsing/remove_things.c
@@ -6,11 +6,44 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 17:02:27 by lgabet            #+#    #+#             */
-/*   Updated: 2023/06/22 12:34:29 by lgabet           ###   ########.fr       */
+/*   Updated: 2023/09/27 14:19:07 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+char	*remove_simple_quotes(char *str)
+{
+	char	*ret;
+	int		i;
+	int		j;
+
+	// i = 0;
+	j = 0;
+	// while (str[i])
+	// {
+	// 	if (str[i] == '"')
+	// 		i++;
+	// 	i++;
+	// }
+	i = ft_strlen(str);
+	ret = calloc((i + 1), sizeof(char));
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'')
+			i++;
+		else
+		{
+			ret[j] = str[i];
+			i++;
+			j++;	
+		}
+	}
+	return (free(str), ret);
+}
 
 char	*remove_quotes(char *str)
 {
@@ -42,5 +75,5 @@ char	*remove_quotes(char *str)
 			j++;	
 		}
 	}
-	return (free(str), ret);
+	return (free(str), remove_simple_quotes(ret));
 }
