eae26a955c46d1d528dffa275d69ad1b590b67cc
root <root@DESKTOP-7CO9QQK>
2023-09-28 14:04:46 +0200

    tester works 35/146

--- src/exec/print_return_value.c
+++ src/exec/print_return_value.c
@@ -5,8 +5,8 @@ void	print_return_value(t_struct *lst)
 	if ((ft_strncmp("$?", lst->str, 2) == 0)
 			&& (ft_strlen(lst->str) == 2))
 	{
-		ft_putnbr_fd(error_value / 256, 0);
-		ft_putstr_fd(": command not found\n", 0);
+		ft_putnbr_fd(error_value / 256, 1);
+		ft_putstr_fd(": command not found\n", 1);
 		exit(127);
 	}
 }
@@ -16,8 +16,8 @@ int	print_clean_return_value(char *str)
 	if ((ft_strncmp("$?", str, 2) == 0)
 			&& (ft_strlen(str) == 2))
 	{
-		ft_putnbr_fd(error_value / 256, 0);
-		ft_putstr_fd("\n", 0);
+		ft_putnbr_fd(error_value / 256, 1);
+		ft_putstr_fd("\n", 1);
 		return (1);
 	}
 	return (0);
