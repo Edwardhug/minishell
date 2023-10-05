cdbe0b598360f34b1b892670411ef1f511f1d3b0
root <root@DESKTOP-7CO9QQK>
2023-10-05 20:31:35 +0200

    trying to add -

--- src/exec/builtins/ft_cd.c
+++ src/exec/builtins/ft_cd.c
@@ -1,16 +1,52 @@
 #include "../../../include/minishell.h"
 
+char	*get_pwd(t_exec *exec)
+{
+	t_env	*env;
+
+	env = exec->lst_env;
+	while (env->next)
+	{
+		if (ft_strcmp(env->name, "PWD") == 0)
+			return (env->value);
+		env = env->next;
+	}
+	return (NULL);
+}
+
+void	change_old_pwd(t_exec *exec, char *pwd)
+{
+	t_env	*env;
+
+	env = exec->lst_env;
+	while (env->next)
+	{
+		if (ft_strcmp(env->name, "OLDPWD") == 0)
+		{
+			free(env->value);
+			env->value = pwd;
+			return ;
+		}
+		env = env->next;
+	}
+}
+
 int	ft_cd(char **cmd, t_exec *exec)
 {
+	char	*pwd;
+
+	pwd = get_pwd(exec);
 	if (ft_strlen_doublechar(cmd) == 1)
-	{
 		chdir("/root");
-	}
 	else if (chdir(cmd[1])) //chdir va tout simplement rediriger vers le chemin donné en argument.
 	{
 		perror("");
 		g_error_value = -33;
+		if (exec->nb_cmds > 1)
+			exit(0);
+		return (0);
 	}
+	change_old_pwd(exec, pwd);
 	if (exec->nb_cmds > 1)
 		exit(0);
 	return (0);
