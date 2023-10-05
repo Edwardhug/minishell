95319919b557be95bd59f33f7d70ebab2c7498ed
root <root@DESKTOP-7CO9QQK>
2023-10-05 18:45:02 +0200

    new struc for cd

--- src/exec/builtins/ft_cd.c
+++ src/exec/builtins/ft_cd.c
@@ -5,11 +5,8 @@ int	ft_cd(char **cmd, t_exec *exec)
 	if (ft_strlen_doublechar(cmd) == 1)
 	{
 		chdir("/root");
-		if (exec->nb_cmds > 1)
-			exit(0);
-		return (0);
 	}
-	if (chdir(cmd[1])) //chdir va tout simplement rediriger vers le chemin donné en argument.
+	else if (chdir(cmd[1])) //chdir va tout simplement rediriger vers le chemin donné en argument.
 	{
 		perror("");
 		g_error_value = -33;
