aa9ed17b413a07c47839bb6404c16c431d605628
root <root@DESKTOP-7CO9QQK>
2023-10-06 17:02:07 +0200

    waiting for export to help for cd

--- src/exec/builtins/ft_cd.c
+++ src/exec/builtins/ft_cd.c
@@ -24,7 +24,28 @@ void	change_old_pwd(t_exec *exec, char *pwd)
 		if (ft_strcmp(env->name, "OLDPWD") == 0)
 		{
 			free(env->value);
-			env->value = pwd;
+			env->value = ft_strdup(pwd);
+			ft_printf("new OLDPWD = %s\n", env->value);
+			return ;
+		}
+		env = env->next;
+	}
+}
+
+void	change_pwd(t_exec *exec)
+{
+	t_env	*env;
+	char	cwd[1024];
+
+	env = exec->lst_env;
+	getcwd(cwd, sizeof(cwd));
+	while (env->next)
+	{
+		if (ft_strcmp(env->name, "PWD") == 0)
+		{
+			free(env->value);
+			env->value = ft_strdup(cwd);
+			ft_printf("new PWD = %s\n", env->value);
 			return ;
 		}
 		env = env->next;
@@ -33,9 +54,9 @@ void	change_old_pwd(t_exec *exec, char *pwd)
 
 int	ft_cd(char **cmd, t_exec *exec)
 {
-	char	*pwd;
+	// char	*pwd;
 
-	pwd = get_pwd(exec);
+	// pwd = get_pwd(exec);
 	if (ft_strlen_doublechar(cmd) == 1)
 		chdir("/root");
 	else if (chdir(cmd[1])) //chdir va tout simplement rediriger vers le chemin donné en argument.
@@ -46,7 +67,9 @@ int	ft_cd(char **cmd, t_exec *exec)
 			exit(0);
 		return (0);
 	}
-	change_old_pwd(exec, pwd);
+	// ft_printf("OLDPWD = %s\n", pwd);
+	// change_old_pwd(exec, pwd);			// need export
+	// change_pwd(exec);					// need export
 	if (exec->nb_cmds > 1)
 		exit(0);
 	return (0);
