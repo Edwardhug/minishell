6896bc534a94bf8cd3dc2280259607e0161fd372
root <root@DESKTOP-7CO9QQK>
2023-10-09 16:05:25 +0200

    new struct for echo

--- src/exec/builtins/ft_echo.c
+++ src/exec/builtins/ft_echo.c
@@ -1,19 +1,20 @@
 #include "../../../include/minishell.h"
 
-void	show_env_var(t_exec *exec, char *arg)
+void	show_env_var(t_exec *exec, char *arg, int *j)
 {
 	t_env	*tmp;
 
 	tmp = exec->env;
 	while (tmp)
 	{
-		if (ft_strcmp(tmp->name, arg) == 0)
+		if (ft_strncmp(tmp->name, arg + 1, ft_strlen(tmp->name)) == 0)
 		{
 			ft_printf("%s", tmp->value);
 			break ;
 		}
 		tmp = tmp->next;
 	}
+	(*j) = (*j) + ft_strlen(tmp->name);
 }
 
 int	is_env_var(t_exec *exec, char *arg)
@@ -34,21 +35,35 @@ int	is_env_var(t_exec *exec, char *arg)
 	}
 	while (tmp)
 	{
-		if (ft_strcmp(tmp->name, arg + 1) == 0)
+		if (ft_strncmp(tmp->name, arg + 1, ft_strlen(tmp->name)) == 0)
+		{
 			return (1);
+		}
 		tmp = tmp->next;
 	}
 	return (0);
+}
+
+void	print_var(char **cmd, int *i, int *j, t_exec *exec)
+{
+	if (cmd[(*i)][(*j) + 1] == '?')
+	{
+		print_clean_return_value(cmd[(*i)] + (*j));
+		(*j)++;
+	}
+	else if (is_env_var(exec, cmd[(*i)] + (*j)))
+		show_env_var(exec, cmd[(*i)] + (*j), j);
+	else
+		ft_printf("$");
 }
 
 int	ft_echo(char **cmd, t_exec *exec)
 {
 	int	dash_n; //sert de booléen
 	int	i;
+	int	j;
 
 	i = 1;
-	if (print_clean_return_value(cmd[1]))
-		exit(0);
 	if (ft_strcmp(cmd[1], "-n") == 0) //on regarde si on a le -n pour le saut à la ligne
 	{
 		i++;
@@ -58,10 +73,15 @@ int	ft_echo(char **cmd, t_exec *exec)
 		dash_n = 1;
 	while (cmd[i])
 	{
-		if (is_env_var(exec, cmd[i]))
-			show_env_var(exec, cmd[i] + 1);
-		else
-			ft_printf("%s", cmd[i]);
+		j = 0;
+		while (cmd[i][j])
+		{
+			if (cmd[i][j] == '$')
+				print_var(cmd, &i, &j, exec);
+			else
+				ft_printf("%c", cmd[i][j]);
+			j++;
+		}
 		if (cmd[i + 1] != NULL) //si on est pas au dernier argument, on met un espace.
 			ft_printf(" ");
 		i++;
