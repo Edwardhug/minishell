54b901ebf9c9656f6730034f579fc7709bd23799
root <root@DESKTOP-7CO9QQK>
2023-10-09 12:41:20 +0200

    fixed error with cd /root/42/rank_3/minishell

--- src/exec/builtins/ft_cd.c
+++ src/exec/builtins/ft_cd.c
@@ -79,7 +79,8 @@ int	ft_cd(char **cmd, t_exec *exec)
 	else if (chdir(cmd[1])) //chdir va tout simplement rediriger vers le chemin donné en argument.
 	{
 		perror("");
-		g_error_value = -1;
+		if (cmd[1][0] != '$' || cmd[2])
+			g_error_value = -1;
 		if (exec->nb_cmds > 1)
 			exit(0);
 		return (0);
