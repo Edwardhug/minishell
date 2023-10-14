9f33fca70c532b7378bfbb5a785c2ed4fa0f9a55
root <root@DESKTOP-7CO9QQK>
2023-10-14 11:43:08 +0200

    strange case protected

--- src/parsing/strange_cmd.c
+++ src/parsing/strange_cmd.c
@@ -1,26 +1,38 @@
 #include "../../include/minishell.h"
 
-int	impair_quote(char *str)
+// int	impair_quote(char *str)
+// {
+// 	int	i;
+// 	int	s_quote;
+// 	int	d_quote;
+
+// 	i = 0;
+// 	s_quote = 0;
+// 	d_quote = 0;
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '\'')
+// 			s_quote++;
+// 		if (str[i] == '\"')
+// 			d_quote++;
+// 		i++;
+// 	}
+// 	if (d_quote % 2 != 0)
+// 		return (1);
+// 	if (s_quote % 2 != 0)
+// 		return (2);
+// 	return (0);
+// }
+
+int	last_char_redir(char *str)
 {
-	int	i;
-	int	s_quote;
-	int	d_quote;
+	int i;
 
 	i = 0;
-	s_quote = 0;
-	d_quote = 0;
-	while (str[i])
-	{
-		if (str[i] == '\'')
-			s_quote++;
-		if (str[i] == '\"')
-			d_quote++;
+	while (str[i] && str[i + 1])
 		i++;
-	}
-	if (d_quote % 2 != 0)
+	if (str[i] == '>' || str[i] == '<' || str[i] == '|')
 		return (1);
-	if (s_quote % 2 != 0)
-		return (2);
 	return (0);
 }
 
@@ -28,9 +40,7 @@ int	have_strange_cmd(char *str)
 {
 	if (str[0] == '|')
 		return (ft_putstr_fd("syntax error near unexpected token `|'\n", 2), g_error_value = 2 * 256, 1);
-	if (impair_quote(str) == 1)
-		return (ft_putstr_fd("syntax error near unexpected token \"\n", 2), g_error_value = 2 * 256, 1);
-	if (impair_quote(str) == 2)
-		return (ft_putstr_fd("syntax error near unexpected token \'\n", 2), g_error_value = 2 * 256, 1);
+	if (last_char_redir(str))
+		return (ft_putstr_fd("syntax error near unexpected token `newline'\n", 2), g_error_value = 2 * 256, 1);
 	return (0);
 }
