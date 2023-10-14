6a94f8b8c2f229af4e341cd48b0d27e97ded0edc
root <root@DESKTOP-7CO9QQK>
2023-10-14 13:25:44 +0200

    signals normed 146/146

--- src/signals/signals_here_doc.c
+++ src/signals/signals_here_doc.c
@@ -6,16 +6,15 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 14:25:52 by lgabet            #+#    #+#             */
-/*   Updated: 2023/10/14 10:23:21 by lgabet           ###   ########.fr       */
+/*   Updated: 2023/10/14 13:24:59 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void sigint_handler_heredoc(int sig)
+void	sigint_handler_heredoc(int sig)
 {
 	(void) sig;
 	ft_putstr_fd("\n", 2);
-	// g_error_value = 130 *256;
 	exit(130);
 }
