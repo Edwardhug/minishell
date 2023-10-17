ed6b2aa6db1a56f435ef832b7f4f9954506d5677
Lucien Gabet <lgabet@z4r8p3.42lyon.fr>
2023-10-17 10:22:41 +0200

    strange redirections like >>> works

--- src/parsing/struct_utils.c
+++ src/parsing/struct_utils.c
@@ -6,7 +6,7 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 14:52:22 by lgabet            #+#    #+#             */
-/*   Updated: 2023/10/17 09:58:00 by lgabet           ###   ########.fr       */
+/*   Updated: 2023/10/17 10:20:59 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,31 @@ t_struct	*get_last_node(t_struct *lst)
 	return (lst);
 }
 
+int	to_long_redir(t_struct *lst)
+{
+	int		i;
+	char	*str;
+
+	while (lst)
+	{
+		i = 0;
+		if (lst->type == ARG)
+		{
+			str = lst->str;
+			while (str[i])
+			{
+				if (str[i] == '>' && str[i + 1] == '>' && str[i + 2] == '>')
+					return (1);
+				if (str[i] == '>' && str[i + 1] == '<')
+					return (1);
+				i++;
+			}
+		}
+		lst = lst->next;
+	}
+	return (0);
+}
+
 int	delete_node(t_struct **lst)
 {
 	t_struct	*tmp;
@@ -65,6 +90,11 @@ int	delete_node(t_struct **lst)
 	{
 		ft_putstr_fd("command not found\n", 2);
 		return (free_list(lst), g_error_value = 127 * 256, 0);
+	}
+	if (to_long_redir(*lst))
+	{
+		ft_putstr_fd("syntax error near unexpected token\n", 2);
+		return (free_list(lst), g_error_value = 2 * 256, 0);
 	}
 	if (ft_strcmp((*lst)->str, "./test_files") == 0 && !(*lst)->next)
 	{
