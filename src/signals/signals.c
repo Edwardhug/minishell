6cce886c8a8e0deeb9842947df2f4aaa84ef4784
Lucien Gabet <lgabet@z4r10p1.42lyon.fr>
2023-10-19 18:53:10 +0200

    signals working in cat

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 14:45:53 by lgabet            #+#    #+#             */
-/*   Updated: 2023/10/16 12:22:23 by lgabet           ###   ########.fr       */
+/*   Updated: 2023/10/19 18:51:55 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,10 @@ void	no_line_return(int sig)
 
 void	sigquit_handler_in_process(int sig)
 {
-	sig_t	old_signal;
-
-	old_signal = signal(SIGINT, no_line_return);
+	ft_putstr_fd("Quit (core dumped)", 2);
 	(void) sig;
 	kill(0, SIGINT);
 	g_error_value = -131;
-	signal(SIGINT, old_signal);
 }
 
 void	sigint_handler_in_process(int sig)
@@ -36,7 +33,7 @@ void	sigint_handler_in_process(int sig)
 
 void	sigint_handler(int sig)
 {
-	printf("\n");
+	ft_printf("\n");
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
