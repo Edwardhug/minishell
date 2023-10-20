77c6b7471fe97aff6ada54dbe04afe57e3a3d3c6
Lucien Gabet <lgabet@z4r9p5.42lyon.fr>
2023-10-20 20:25:06 +0200

    remove_things normed

--- src/parsing/remove_things.c
+++ src/parsing/remove_things.c
@@ -6,7 +6,7 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 17:02:27 by lgabet            #+#    #+#             */
-/*   Updated: 2023/10/14 13:04:36 by lgabet           ###   ########.fr       */
+/*   Updated: 2023/10/20 20:24:41 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,21 @@ char	*have_dollar_sign(char *str, char *ret, int *i, int *j)
 	return (ret);
 }
 
+int	remove_simple_quote_loop(char *str, char **ret, int *i, int *j)
+{
+	if (str[(*i)] == '\'' && (str[(*i) + 1] == '$'))
+		return (free((*ret)), 1);
+	if (str[(*i)] == '\'')
+		(*i)++;
+	else
+	{
+		(*ret)[(*j)] = str[(*i)];
+		(*i)++;
+		(*j)++;
+	}
+	return (0);
+}
+
 char	*remove_simple_quotes(char *str, t_exec *exec)
 {
 	char	*ret;
@@ -44,18 +59,25 @@ char	*remove_simple_quotes(char *str, t_exec *exec)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == '\'' && (str[i + 1] == '$'))
-			return (free(ret), str);
-		if (str[i] == '\'')
-			i++;
-		else
-		{
-			ret[j] = str[i];
-			i++;
-			j++;
-		}
+		if (remove_simple_quote_loop(str, &ret, &i, &j))
+			return (str);
 	}
 	return (free(str), ret);
+}
+
+void	remove_quote_loop(char *str, char **ret, int *i, int *j)
+{
+	if (str[(*i)] == '"')
+	{
+		(*ret) = have_dollar_sign(str, *ret, i, j);
+		(*i)++;
+	}
+	else
+	{
+		(*ret)[(*j)] = str[(*i)];
+		(*i)++;
+		(*j)++;
+	}
 }
 
 char	*remove_quotes(char *str, t_exec *exec)
@@ -74,18 +96,6 @@ char	*remove_quotes(char *str, t_exec *exec)
 	}
 	i = 0;
 	while (str[i])
-	{
-		if (str[i] == '"')
-		{
-			ret = have_dollar_sign(str, ret, &i, &j);
-			i++;
-		}
-		else
-		{
-			ret[j] = str[i];
-			i++;
-			j++;
-		}
-	}
+		remove_quote_loop(str, &ret, &i, &j);
 	return (free(str), remove_simple_quotes(ret, exec));
 }
